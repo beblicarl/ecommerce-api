39b0f2854c7108ce540df35c4be314edba98ce2a
Carl Bebli <beblicarl.cb@gmail.com>
2023-01-18 18:57:08 +0000

    i created a .env file which includes my api keys and then i accessed them in the config/index.js

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+    PORT: process.env.PORT,
+    MONGODB_CONNECTION_URL: process.env.MONGODB_CONNECTION_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+};
