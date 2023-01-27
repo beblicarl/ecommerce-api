6d166b78d4b231983a13bdc5b31fd1d9344c749f
Carl Bebli <beblicarl.cb@gmail.com>
2023-01-27 15:03:08 +0000

    created a file to export the models

---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+const UserModel = require("./user.model");
+const ProductModel = require("./product.model")
+const CartModel = require("./cart.model")
+
+module.exports = {
+  UserModel,
+  ProductModel,
+  CartModel
+};
