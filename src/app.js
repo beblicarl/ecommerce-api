1e7431061491a9fc44bee85f1dd680cf4fbe35d3
Carl Bebli <beblicarl.cb@gmail.com>
2023-01-27 14:58:34 +0000

    imported middleware in app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,14 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const cors = require("cors");
 
-const CONFIG = require("./src/config");
+require("dotenv").config();
+const connecDB = require("./database")
+// const CONFIG = require("./src/config");
+const router = require("./routes")
+
+
+require("./lib/passport")
+
 
 const app = express();
 
@@ -20,17 +27,21 @@ app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  fileUpload({
-    createParentPath: true,
-    useTempFiles: true,
-    tempFileDir: "/tmp/",
-  })
-);
+// app.use(
+//   fileUpload({
+//     createParentPath: true,
+//     useTempFiles: true,
+//     tempFileDir: "/tmp/",
+//   })
+// );
+connecDB()
 
 app.get("/", (req, res) => {
     return res.json({ status: true });
 });
+
+// Routes
+app.use('/api/v1', router)
 
 // 404 error handler
 app.all("*", (req, res, next) => {
