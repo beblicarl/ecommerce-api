81b42263d884db4fdc70ff2e9e96d02c845e26f5
Carl Bebli <beblicarl.cb@gmail.com>
2023-01-27 16:03:14 +0000

    fixed the strictQuery error message in the console

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = async function connecDB() {
+  await mongoose.set("strictQuery", false);
   await mongoose.connect(process.env.MONGODB_CONNECTION_URL, () => {
     console.log(`MongoDB Database Connected`);
   });
