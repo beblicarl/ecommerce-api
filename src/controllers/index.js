0b6e6d5f6895f7083a4d2eccc1540a299c12b64f
Carl Bebli <beblicarl.cb@gmail.com>
2023-01-31 16:48:36 +0000

    imported the cartController in the index.js fle and exported it

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const authController = require("./auth.controller"); 
 const productController = require("./product.controller")
+const cartController = require("./cart.controller")
 
 module.exports = { 
     authController,
-    productController
+    productController,
+    cartController
 };
