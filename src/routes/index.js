c14d8689dfd9189ba7bb3be663bcaf70aeffcedf
Carl Bebli <beblicarl.cb@gmail.com>
2023-01-31 16:49:06 +0000

    imported the cartRouter and gave it a base url of cart

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@
 
  const authRouter = require('./auth')
  const productRouter = require('./product')
+ const cartRouter = require('./cart')
  
  const app = express.Router()
  
  app.use('/auth', authRouter)
  app.use('/product', productRouter)
+ app.use('/cart', cartRouter)
 
  
  module.exports = app
